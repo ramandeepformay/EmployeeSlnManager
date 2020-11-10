2bbd3c46048f623fda1d8098422380adba20c5b0
Ramandeep Formay <ramandeepformay@gmail.com>
2020-11-10 16:42:59 -0600

    added employeemanager engine to  the web api

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using EmployeeCrudManager;
+using EmployeeCrudManager.Models;
+using EmployeeCrudManager.Storage;
+
+namespace EmployeeWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+       private EmpMgtSys _eManager;
+       public EmployeeController(){
+           var employeeStorage =new EmpStorageLists();
+            
+            Console.WriteLine("Welcome to the Employee Manager!!\n");
+
+            // creating an instance of employee mgt storage which is the main class
+             _eManager = new EmpMgtSys(employeeStorage);
+
+            // user input class's input method called
+            // EmpUserInput.Input(eManager);
+        
+       }
+        [HttpGet]
+        public List<EmployeeInformation> Get()
+        {
+           return _eManager.Print();
+        }
+        // [HttpGet]
+        // public EmployeeInformation Search(string input){
+        //     return _eManager.Search(input);
+        // }
+    }
+}
